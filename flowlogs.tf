a06bfe78377ca05adb4953f1bbea503268d1e7b3
Akshai <akshay05pakhanati@gmail.com>
2024-02-22 04:07:42 +0530

    explicit and implicit

--- flowlogs.tf
+++ flowlogs.tf
@@ -1,20 +1,29 @@
-#resource "aws_flow_log" "example" {
-#  log_destination      = aws_s3_bucket.example.arn
-#  log_destination_type = "s3"
-#  traffic_type         = "ALL"
-#  vpc_id               = aws_vpc.example.id
-#}
-#
-#resource "aws_s3_bucket" "akshaivpcflowlogs" {
-#  bucket = "akshaivpcflowlogs"
-#}
+resource "aws_flow_log" "example" {
+  log_destination      = aws_s3_bucket.akshaivpcflowlogs.arn
+  log_destination_type = "s3"
+  traffic_type         = "ALL"
+  vpc_id               = aws_vpc.default.id
+}
+
+resource "aws_s3_bucket" "akshaivpcflowlogs" {
+  bucket = "akshaivpcflowlogs"
+  depends_on = [
+    aws_s3_bucket.akshaivpc1
+  ]
+}
 
 resource "aws_s3_bucket" "akshaivpc1" {
   bucket = "akshaivpc1"
+  depends_on = [
+    aws_s3_bucket.akshaivpc2
+  ]
 }
 
 resource "aws_s3_bucket" "akshaivpc2" {
   bucket = "akshaivpc2"
+  depends_on = [
+    aws_s3_bucket.akshaivpc3
+  ]
 }
 
 resource "aws_s3_bucket" "akshaivpc3" {

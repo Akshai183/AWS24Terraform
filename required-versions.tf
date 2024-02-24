2d91309a844eeb8fe60ea364a02bc1d24d25c3db
Akshai <akshay05pakhanati@gmail.com>
2024-02-24 18:35:50 +0530

    dynamodb

--- required-versions.tf
+++ required-versions.tf
@@ -14,3 +14,12 @@ terraform {
     region = "us-east-1"
   }
 }
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "akshai-state-lock-dynamo"
+  hash_key = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}

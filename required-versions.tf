bc746f5f19a3048d721a7b3ba1f7bab1a5970354
Akshai <akshay05pakhanati@gmail.com>
2024-02-25 02:46:47 +0530

    added Meta arguments create before destroy

--- required-versions.tf
+++ required-versions.tf
@@ -1,14 +1,4 @@
 terraform {
-  required_version = "= 1.7.4" #Forcing which version of Terraform needs to be used
-  required_providers {
-    aws = {
-      version = "<= 5.0.0" #Forcing which version of plugin needs to be used.
-      source  = "hashicorp/aws"
-    }
-  }
-}
-
-terraform {
   backend "s3" {
     bucket = "akshai183statefile"
     #dynamodb_table = "akshai-state-lock-dynamo"
@@ -16,4 +6,3 @@ terraform {
     region = "us-east-1"
   }
 }
-

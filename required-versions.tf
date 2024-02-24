5a3b382606573de1f5fdd36a0d195338953801e5
Akshai <akshay05pakhanati@gmail.com>
2024-02-24 18:42:01 +0530

    dynamodb -auto locking terraform

--- required-versions.tf
+++ required-versions.tf
@@ -10,6 +10,7 @@ terraform {
 terraform {
   backend "s3" {
     bucket = "akshai183statefile"
+    dynamodb_table = "akshai-state-lock-dynamo"
     key    = "akshai.tfstate"
     region = "us-east-1"
   }
